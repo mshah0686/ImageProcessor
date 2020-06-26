6abbc01e9a69a351b48bccfad209948efa71292b
mshah0686 <mshah0686@gmail.com>
2020-06-26 00:51:21 -0500

    Working previous image feature

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class choice:
         self.image = image
 
 original_image_path = "temp.jpg"
-choices = ("Fourier", "Fourier Zero Shifted", "LPF", "HPF", "Gaussian LPF", "Gaussian HPF")
+choices = ("Fourier", "Fourier Zero Shifted", "Ideal LPF", "Ideal HPF", "Gaussian LPF", "Gaussian HPF")
 filter_choices = []
 
 # Add option to upload an image or use default image
@@ -30,21 +30,31 @@ filter_choices = []
 def run_analysis():
     global label, root, filter_choices
     print('button pressed -> loading image')
-    original_np = cv.imread(original_image_path, 0)
-    fourier_np = np.fft.fft2(original_np)
-    shifted_np = np.fft.fftshift(fourier_np)
+    input_np = cv.imread(original_image_path, 0)
     total_filters = 1
     for f in filter_choices:
         total_filters += f.use_filter.get()
     next_plot = 0
 
     #Find analysis to use
-    for f in filter_choices:
+    for i in range(len(filter_choices)):
+        f = filter_choices[i]
+        #use if filter being applied
         if f.use_filter.get():
+            original_np= []
+            if f.use_previous.get() and i >0:
+                original_np = filter_choices[i-1].image
+            else:
+                original_np = input_np
+            #calculuate fourier and shifts
+            fourier_np = np.fft.fft2(original_np)
+            shifted_np = np.fft.fftshift(fourier_np)
             analysis = f.selection.get()
-            print(original_np.shape[0])
+
+            #Scaled image radius based on image size
             scaled_size = (f.param.get() / 200) * original_np.shape[0]
-            if analysis == 'LPF':
+
+            if analysis == 'Ideal LPF':
                 #LOW PASS FILTER: Original, fourier, Fourier shift, LPF mask, Inverse
                 graph_np = shifted_np * filters.idealFilterLP(scaled_size, original_np.shape)
                 spatial_image = np.fft.ifft2(graph_np)
@@ -53,9 +63,8 @@ def run_analysis():
                 plt.subplot(total_filters, 5, next_plot * 5 + 3), plt.imshow(np.log(1+np.abs(shifted_np)), "gray"), plt.title('zero shift')
                 plt.subplot(total_filters, 5, next_plot * 5 + 4), plt.imshow(np.log(1+np.abs(graph_np)), "gray"), plt.title('mask')
                 plt.subplot(total_filters, 5, next_plot * 5 + 5), plt.imshow(np.log(1+np.abs(spatial_image)), "gray"), plt.title('LPF')
-                f.image = spatial_image
-            
-            elif analysis == 'HPF':
+                f.image = np.log(1+np.abs(spatial_image))
+            elif analysis == 'Ideal HPF':
                 #HIGH PASS FILTER: Original, fourier, Fourier shift, HPF mask, Inverse
                 graph_np = shifted_np * filters.idealFilterHP(scaled_size, original_np.shape)
                 spatial_image = np.fft.ifft2(graph_np)
@@ -64,7 +73,7 @@ def run_analysis():
                 plt.subplot(total_filters, 5, next_plot * 5 + 3), plt.imshow(np.log(1+np.abs(shifted_np)), "gray"), plt.title('zero shift')
                 plt.subplot(total_filters, 5, next_plot * 5 + 4), plt.imshow(np.log(1+np.abs(graph_np)), "gray"), plt.title('mask')
                 plt.subplot(total_filters, 5, next_plot * 5 + 5), plt.imshow(np.log(1+np.abs(spatial_image)), "gray"), plt.title('HPF')
-                f.image = spatial_image
+                f.image = np.log(1+np.abs(spatial_image))
             
             elif analysis == 'Gaussian LPF':
                 #Gaussian LPF: Original, fourier, Fourier Shift, Mask, Inverse
@@ -75,7 +84,7 @@ def run_analysis():
                 plt.subplot(total_filters, 5, next_plot * 5 + 3), plt.imshow(np.log(1+np.abs(shifted_np)), "gray"), plt.title('zero shift')
                 plt.subplot(total_filters, 5, next_plot * 5 + 4), plt.imshow(np.log(1+np.abs(graph_np)), "gray"), plt.title('mask')
                 plt.subplot(total_filters, 5, next_plot * 5 + 5), plt.imshow(np.log(1+np.abs(spatial_image)), "gray"), plt.title('Gaussian LPF')
-                f.image = spatial_image
+                f.image = np.log(1+np.abs(spatial_image))
             
             elif analysis == 'Gaussian HPF':
                 #Gaussian HPF: Original, fourier, Fourier Shift, Mask, Inverse
@@ -86,18 +95,20 @@ def run_analysis():
                 plt.subplot(total_filters, 5, next_plot * 5 + 3), plt.imshow(np.log(1+np.abs(shifted_np)), "gray"), plt.title('zero shift')
                 plt.subplot(total_filters, 5, next_plot * 5 + 4), plt.imshow(np.log(1+np.abs(graph_np)), "gray"), plt.title('mask')
                 plt.subplot(total_filters, 5, next_plot * 5 + 5), plt.imshow(np.log(1+np.abs(spatial_image)), "gray"), plt.title('Gaussian HPF')
-                f.image = spatial_image
+                f.image = np.log(1+np.abs(spatial_image))
             
             elif analysis == 'Fourier':
                 #Fourier
                 plt.subplot(total_filters, 5, next_plot * 5 + 1), plt.imshow(original_np, "gray"), plt.title('original')
                 plt.subplot(total_filters, 5, next_plot * 5 + 2), plt.imshow(np.log(1+np.abs(fourier_np)), "gray"), plt.title('fourier')
+                f.image = original_np
             
             elif analysis == 'Fourier Zero Shifted':
                 #Fourier, zero shift
                 plt.subplot(total_filters, 5, next_plot * 5 + 1), plt.imshow(original_np, "gray"), plt.title('original')
                 plt.subplot(total_filters, 5, next_plot * 5 + 2), plt.imshow(np.log(1+np.abs(fourier_np)), "gray"), plt.title('fourier')
                 plt.subplot(total_filters, 5, next_plot * 5 + 3), plt.imshow(np.log(1+np.abs(shifted_np)), "gray"), plt.title('zero shift')
+                f.image = origional_np
             
             elif analysis == 'Edge Detection':
                 #TODO: implement edge detection
@@ -113,10 +124,10 @@ next_loc = 1
 def add_filter():
     global filter_choices, root, next_loc
     #Variables per filter
-    var = IntVar()
+    var = IntVar(value=1)
     var2 = IntVar(value=1)
     stringvar = StringVar()
-    param = IntVar()
+    param = IntVar(value = 25)
 
     #add filter to array as choice object
     filter_choices.append( choice(var, stringvar, var2, param) )
@@ -132,7 +143,7 @@ def add_filter():
     Checkbutton(root, text="Use Previous Filter as Input", var = var2).grid(row = 2, column = next_loc)
 
     #Input for parameter selection
-    slider = Scale(from_=0, to=100, resolution=0.1, label="Size (%)(if applicable)", orient=HORIZONTAL, length=145, variable = param)
+    slider = Scale(from_=0, to=100, resolution=0.1, label="Size(%)(if applicable):", orient=HORIZONTAL, length=145, variable = param)
     slider.grid(row = 3, column = next_loc)
 
     #Update location
