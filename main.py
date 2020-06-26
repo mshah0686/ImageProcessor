1245de15fd2e4dbd6f55e378f10a740336ddea26
mshah0686 <mshah0686@gmail.com>
2020-06-25 22:57:41 -0500

    Working filters and dynamic sizing

--- main.py
+++ main.py
@@ -8,65 +8,140 @@ from tkinter import ttk
 import filters
 
 class choice:
-    def __init__(self, checkbox, selection, previous):  
+    def __init__(self, checkbox, selection, previous, param):  
         self.use_filter = checkbox
         self.selection = selection
         self.use_previous = previous
+        self.param = param
+        self.image = None
+    def setImage(self, image):
+        self.image = image
 
 original_image_path = "temp.jpg"
-choices = ("Fourier", "LPF", "HPF")
+choices = ("Fourier", "Fourier Zero Shifted", "LPF", "HPF", "Gaussian LPF", "Gaussian HPF")
 filter_choices = []
 
-#button press
+# Add option to upload an image or use default image
+# Add edge detection choices
+# Add using previous image checkbox functionality
+# Add compression filters -> lossless and lossy
+
+# Run analysis button function -> display the analysis on matplotlib
 def run_analysis():
     global label, root, filter_choices
     print('button pressed -> loading image')
     original_np = cv.imread(original_image_path, 0)
     fourier_np = np.fft.fft2(original_np)
     shifted_np = np.fft.fftshift(fourier_np)
-    plt.subplot(2, 5, 1), plt.imshow(original_np, "gray"), plt.title("Original Image")
-    next_plot = 2
-    #plt.subplot(2, 5, 2), plt.imshow(np.log(1+np.abs(fourier_np)), "gray"), plt.title("Fourier Transform")
-    #plt.subplot(2, 5, 3), plt.imshow(np.log(1+np.abs(shifted_np)), "gray"), plt.title("Fourier Shifted")
+    total_filters = 1
+    for f in filter_choices:
+        total_filters += f.use_filter.get()
+    next_plot = 0
 
     #Find analysis to use
     for f in filter_choices:
         if f.use_filter.get():
             analysis = f.selection.get()
-            graph_np = None
+            print(original_np.shape[0])
+            scaled_size = (f.param.get() / 200) * original_np.shape[0]
             if analysis == 'LPF':
-                print('using lpf')
-                graph_np = shifted_np * filters.idealFilterLP(10, original_np.shape)
+                #LOW PASS FILTER: Original, fourier, Fourier shift, LPF mask, Inverse
+                graph_np = shifted_np * filters.idealFilterLP(scaled_size, original_np.shape)
+                spatial_image = np.fft.ifft2(graph_np)
+                plt.subplot(total_filters, 5, next_plot * 5 + 1), plt.imshow(original_np, "gray"), plt.title('original')
+                plt.subplot(total_filters, 5, next_plot * 5 + 2), plt.imshow(np.log(1+np.abs(fourier_np)), "gray"), plt.title('fourier')
+                plt.subplot(total_filters, 5, next_plot * 5 + 3), plt.imshow(np.log(1+np.abs(shifted_np)), "gray"), plt.title('zero shift')
+                plt.subplot(total_filters, 5, next_plot * 5 + 4), plt.imshow(np.log(1+np.abs(graph_np)), "gray"), plt.title('mask')
+                plt.subplot(total_filters, 5, next_plot * 5 + 5), plt.imshow(np.log(1+np.abs(spatial_image)), "gray"), plt.title('LPF')
+                f.image = spatial_image
+            
             elif analysis == 'HPF':
-                print('using hpf')
-                graph_np = shifted_np * filters.idealFilterHP(10, original_np.shape)
+                #HIGH PASS FILTER: Original, fourier, Fourier shift, HPF mask, Inverse
+                graph_np = shifted_np * filters.idealFilterHP(scaled_size, original_np.shape)
+                spatial_image = np.fft.ifft2(graph_np)
+                plt.subplot(total_filters, 5, next_plot * 5 + 1), plt.imshow(original_np, "gray"), plt.title('original')
+                plt.subplot(total_filters, 5, next_plot * 5 + 2), plt.imshow(np.log(1+np.abs(fourier_np)), "gray"), plt.title('fourier')
+                plt.subplot(total_filters, 5, next_plot * 5 + 3), plt.imshow(np.log(1+np.abs(shifted_np)), "gray"), plt.title('zero shift')
+                plt.subplot(total_filters, 5, next_plot * 5 + 4), plt.imshow(np.log(1+np.abs(graph_np)), "gray"), plt.title('mask')
+                plt.subplot(total_filters, 5, next_plot * 5 + 5), plt.imshow(np.log(1+np.abs(spatial_image)), "gray"), plt.title('HPF')
+                f.image = spatial_image
+            
+            elif analysis == 'Gaussian LPF':
+                #Gaussian LPF: Original, fourier, Fourier Shift, Mask, Inverse
+                graph_np = shifted_np * filters.gaussianLP(scaled_size, original_np.shape)
+                spatial_image = np.fft.ifft2(graph_np)
+                plt.subplot(total_filters, 5, next_plot * 5 + 1), plt.imshow(original_np, "gray"), plt.title('original')
+                plt.subplot(total_filters, 5, next_plot * 5 + 2), plt.imshow(np.log(1+np.abs(fourier_np)), "gray"), plt.title('fourier')
+                plt.subplot(total_filters, 5, next_plot * 5 + 3), plt.imshow(np.log(1+np.abs(shifted_np)), "gray"), plt.title('zero shift')
+                plt.subplot(total_filters, 5, next_plot * 5 + 4), plt.imshow(np.log(1+np.abs(graph_np)), "gray"), plt.title('mask')
+                plt.subplot(total_filters, 5, next_plot * 5 + 5), plt.imshow(np.log(1+np.abs(spatial_image)), "gray"), plt.title('Gaussian LPF')
+                f.image = spatial_image
+            
+            elif analysis == 'Gaussian HPF':
+                #Gaussian HPF: Original, fourier, Fourier Shift, Mask, Inverse
+                graph_np = shifted_np * filters.gaussianHP(scaled_size, original_np.shape)
+                spatial_image = np.fft.ifft2(graph_np)
+                plt.subplot(total_filters, 5, next_plot * 5 + 1), plt.imshow(original_np, "gray"), plt.title('original')
+                plt.subplot(total_filters, 5, next_plot * 5 + 2), plt.imshow(np.log(1+np.abs(fourier_np)), "gray"), plt.title('fourier')
+                plt.subplot(total_filters, 5, next_plot * 5 + 3), plt.imshow(np.log(1+np.abs(shifted_np)), "gray"), plt.title('zero shift')
+                plt.subplot(total_filters, 5, next_plot * 5 + 4), plt.imshow(np.log(1+np.abs(graph_np)), "gray"), plt.title('mask')
+                plt.subplot(total_filters, 5, next_plot * 5 + 5), plt.imshow(np.log(1+np.abs(spatial_image)), "gray"), plt.title('Gaussian HPF')
+                f.image = spatial_image
+            
             elif analysis == 'Fourier':
-                print('using fourier')
-                graph_np = shifted_np
-            else:
-                print('Nothing')
-            plt.subplot(2, 5, next_plot), plt.imshow(np.log(1+np.abs(graph_np)), "gray"), plt.title(analysis)
+                #Fourier
+                plt.subplot(total_filters, 5, next_plot * 5 + 1), plt.imshow(original_np, "gray"), plt.title('original')
+                plt.subplot(total_filters, 5, next_plot * 5 + 2), plt.imshow(np.log(1+np.abs(fourier_np)), "gray"), plt.title('fourier')
+            
+            elif analysis == 'Fourier Zero Shifted':
+                #Fourier, zero shift
+                plt.subplot(total_filters, 5, next_plot * 5 + 1), plt.imshow(original_np, "gray"), plt.title('original')
+                plt.subplot(total_filters, 5, next_plot * 5 + 2), plt.imshow(np.log(1+np.abs(fourier_np)), "gray"), plt.title('fourier')
+                plt.subplot(total_filters, 5, next_plot * 5 + 3), plt.imshow(np.log(1+np.abs(shifted_np)), "gray"), plt.title('zero shift')
+            
+            elif analysis == 'Edge Detection':
+                #TODO: implement edge detection
+                pass
+            
+            elif analysis == 'Compression':
+                pass
         next_plot = next_plot + 1
     plt.show()
-
 next_loc = 1
 
+# Adding filter option to the window
 def add_filter():
     global filter_choices, root, next_loc
+    #Variables per filter
     var = IntVar()
-    var2 = IntVar()
+    var2 = IntVar(value=1)
     stringvar = StringVar()
-    filter_choices.append( choice(var, stringvar, var2) )
-    Checkbutton(root, text="use", var=var).grid(row=0, column = next_loc)
-    Checkbutton(root, text="use previous image", var = var2).grid(row = 2, column = next_loc)
+    param = IntVar()
+
+    #add filter to array as choice object
+    filter_choices.append( choice(var, stringvar, var2, param) )
+
+    #Add check box for applying filter or not
+    Checkbutton(root, text="Apply Filter", var=var).grid(row=0, column = next_loc)
+
+    #Combobox for filter choices
     dp = ttk.Combobox(root, state="readonly", textvariable = stringvar, value=choices)
     dp.grid(row = 1, column = next_loc)
+
+    #Add check box for using previous or original
+    Checkbutton(root, text="Use Previous Filter as Input", var = var2).grid(row = 2, column = next_loc)
+
+    #Input for parameter selection
+    slider = Scale(from_=0, to=100, resolution=0.1, label="Size (%)(if applicable)", orient=HORIZONTAL, length=145, variable = param)
+    slider.grid(row = 3, column = next_loc)
+
+    #Update location
     next_loc = next_loc + 1
     print('Add fliter')
 
 #create application and set size
 root = Tk()
-root.geometry("1200x100")
+root.geometry("1200x150")
 
 #add a button
 but = Button(root, command = run_analysis, text = 'Analyze')
